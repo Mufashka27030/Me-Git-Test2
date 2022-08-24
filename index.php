7fe73699059e2e9cb080186645cd69f9470ab494
Muhammad Nabil <mn2237942@gmail.com>
2022-08-24 09:52:17 +0700

    Menambahkan Paragaf Baru Pada File index.html

--- index.php
+++ index.php
@@ -8,5 +8,15 @@
     </head>
     <body>
         <h1>Halo Semua</h1>
+        <p>
+        An Internet meme, more commonly known simply as a meme 
+        (/miːm/ MEEM), is an idea, behavior, image, or style that 
+        is spread via the Internet, often through social media platforms. 
+        What is considered a meme may vary across different communities on the 
+        Internet and is subject to change over time. Traditionally, they were a concept 
+        or catchphrase, but the concept has since become broader and more multi-faceted, 
+        evolving to include more elaborate structures such as challenges, GIFs, videos, and viral 
+        sensations.[1]
+        </p>    
     </body>
 </html>
